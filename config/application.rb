2c13810a25d52f90f55b32978449d701fcec5e9f
mzmt <mrcrius1355@gmail.com>
2018-03-23 03:44:14 +0000

    test

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module twimark
+module Twimark
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.assets.initialize_on_precompile = false
